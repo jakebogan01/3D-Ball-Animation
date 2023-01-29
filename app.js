bbd17f4c797308f8e704ec0e6b21b8008669499c
Jake Bogan <jakebogan01@gmail.com>
2023-01-28 22:08:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+const frameCount = 179;
+
+const currentFrame = (index) => `./balls/${(index + 1).toString()}.jpg`;
+
+const images = [];
+let ball = { frame: 0 };
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    console.log(currentFrame(i));
+    images.push(img);
+}
+
+gsap.to(ball, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger: {
+        scrub: 0.5,
+        pin: "canvas",
+        end: "500%",
+    },
+    onUpdate: render,
+});
+
+gsap.fromTo(
+    ".ball-text",
+    {
+        opacity: 0,
+    },
+    {
+        opacity: 1,
+        scrollTrigger: {
+            scrub: 1,
+
+            start: "50%",
+            end: "60%",
+        },
+        onComplete: () => {
+            gsap.to(".balls", { opacity: 0 });
+        },
+    }
+);
+
+images[0].onload = render;
+
+function render() {
+    context.canvas.width = images[0].width;
+    context.canvas.height = images[0].height;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[ball.frame], 0, 0);
+}
